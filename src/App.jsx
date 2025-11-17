0dfb1f6f10d500b39b2ec92abf05319613860dee
WaraneyKamagi <abrahamkamagi16@gmail.com>
2025-11-17 09:31:31 +0800

    tambah header

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import Hobi from "./Components/Hobi";
 import Skills from "./Components/Skills";
 import Contact from "./Components/Contact";
 import Projects from './Components/Projects.jsx';
+import Header from './Components/Header.jsx';
 
 const App = () => {
   return (
     <div className="font-sans">
+      <Header/>
       <Hero />
       <About />
       <Hobi />
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="w-full bg-white shadow-sm">
+      <nav className="w-full flex justify-between items-center py-4 px-6">
+        <h1 className="text-2xl font-bold text-blue-600">
+          MyPortfolio
+        </h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
